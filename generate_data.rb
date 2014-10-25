bbf19992b162a7c07eb41412512f9f7066d392b1
michail <michailgames@yahoo.pl>
2014-10-26 00:50:10 +0200

    created script for populating database

--- generate_data.rb
+++ generate_data.rb
@@ -3,3 +3,44 @@ ol.quantity = 4
 ol.order_id = 1
 ol.product_id = 1
 ol.save!
+
+ol = OrderLine.new
+ol.quantity = 1
+ol.order_id = 1
+ol.product_id = 2
+ol.save!
+
+ol = OrderLine.new
+ol.quantity = 3
+ol.order_id = 2
+ol.product_id = 3
+ol.save!
+
+ol = OrderLine.new
+ol.quantity = 1
+ol.order_id = 2
+ol.product_id = 1
+ol.save!
+
+p = Product.new
+p.name = "Chair"
+p.price = 19.99
+p.save!
+
+p = Product.new
+p.name = "Table"
+p.price = 89.99
+p.save!
+
+p = Product.new
+p.name = "Carpet"
+p.price = "49.99"
+p.save!
+
+p = Order.new
+p.date = Date.new(2014, 10, 25)
+p.save!
+
+p = Order.new
+p.date = Date.new(2014, 10, 26)
+p.save!
